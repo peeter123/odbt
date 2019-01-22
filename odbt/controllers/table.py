4fece283024f86bbc4c843e6153dc313e45629a1
Peter Oostewechel <peter.oostewechel@ict.nl>
2019-01-22 08:59:51 +0100

    Added ability to list a table

--- odbt/controllers/table.py
+++ odbt/controllers/table.py
@@ -1,5 +1,6 @@
 from cement import Controller, ex
 
+
 class Table(Controller):
     class Meta:
         label = 'table'
@@ -12,6 +13,29 @@ class Table(Controller):
         # text displayed at the bottom of --help output
         epilog = 'Usage: odbt table --foo bar'
 
+        # controller level arguments. ex: 'odbt --version'
+        arguments = [
+            ### add a version banner
+            (['-t', '--table'],
+             {'help': 'The table where the item should be stored',
+              'action': 'store',
+              'dest': 'table'}),
+            (['-y', '--non-interactive'],
+             {'help': 'Run in non-interactive mode',
+              'action': 'store_true',
+              'dest': 'non_interactive'}),
+        ]
+
+    def _check_table_valid(self):
+        # Get the table list from the database and strip lookup tables
+        table_list = list(self.app.db.tables(tableType='TABLE'))
+        table_list = [x for x in table_list if 'lookup_' not in x.table_name]
+
+        if self.app.pargs.table is None or self.app.pargs.table not in [x.table_name for x in table_list]:
+            self.app.print('Please provide the correct table name')
+            self.app.exit_code = 1
+            self.app.close()
+
     def _default(self):
         """Default action if no sub-command is passed."""
 
@@ -21,18 +45,14 @@ class Table(Controller):
         help='List items from a specific table',
 
         # sub-command level arguments. ex: 'odbt command1 --foo bar'
-        arguments=[
-            ### add a sample foo option under subcommand namespace
-            (['table'],
-             {'help': 'The table where the item should be stored',
-              'action': 'store'}),
-        ],
+        arguments=[],
     )
     def list(self):
         """List database table"""
 
         ### do something with arguments
         if self.app.pargs.table is not None:
+            self._check_table_valid()
             data = {'data': self.app.db.execute('select * from ' + self.app.pargs.table).fetchall()}
             self.app.render(data, 'table-list.jinja2')
         else:
